f8df54312f5d06d0f8e5bfefdc8c169db20c4638
brunobahri <brunobahri27@gmail.com>
2024-07-15 20:49:52 -0300

    feat: controller for create user and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
