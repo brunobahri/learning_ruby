d8aa7bf4c982fc27ffbc2031f2aa770c610a03ea
brunobahri <brunobahri27@gmail.com>
2024-07-16 23:57:23 -0300

    feat: jwt_denied table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# config/routes.rb
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -5,4 +6,8 @@ Rails.application.routes.draw do
   }
 
   resources :tasks
+
+  # Garantir que o login e logout utilizem POST e DELETE respectivamente
+  post 'login', to: 'users/sessions#create'
+  delete 'logout', to: 'users/sessions#destroy'
 end
--- db/migrate/20240715004305_create_jwt_denylists.rb
+++ db/migrate/20240715004305_create_jwt_denylists.rb
@@ -1,8 +1,8 @@
 class CreateJwtDenylists < ActiveRecord::Migration[7.1]
   def change
     create_table :jwt_denylists do |t|
-      t.string :jti
-      t.datetime :exp
+      t.string :jti, null: false
+      t.datetime :exp, null: false
 
       t.timestamps
     end
