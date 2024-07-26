aae03a348961e0c5ef4fe66bb3401f4a5014f05b
brunobahri <brunobahri27@gmail.com>
2024-07-25 22:34:20 -0300

    test: tests for security user create, login and logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'database_cleaner/active_record'
+require 'securerandom'
 
 module ActiveSupport
   class TestCase
@@ -9,5 +11,19 @@ module ActiveSupport
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
+
+    # Configuração do DatabaseCleaner
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+
+    # Limpa o banco de dados antes de todos os testes
+    setup do
+      DatabaseCleaner.start
+    end
+
+    # Limpa o banco de dados após cada teste
+    teardown do
+      DatabaseCleaner.clean
+    end
   end
 end
