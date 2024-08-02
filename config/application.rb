b801829f28e914a827947ec09ae33c2a80732088
brunobahri <brunobahri27@gmail.com>
2024-08-02 13:24:11 -0300

    fix: removing tests for throtling limit

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
+# config/application.rb
 require_relative "boot"
 
-# Require only the frameworks you want to use, instead of requiring "rails/all"
+require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -8,52 +9,30 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Require the custom middleware
 require_relative "../lib/middleware/force_json_format"
 
 module TaskManager
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Only loads a smaller set of middleware suitable for API only apps.
     config.api_only = true
 
-    # Add lib/middleware to the autoload paths
     config.autoload_paths += %W(#{config.root}/lib/middleware)
 
-    # Add the ForceJsonFormat middleware
     config.middleware.use ForceJsonFormat
-
-    # Add the Rack::Attack middleware
     config.middleware.use Rack::Attack
 
-    # Configurar um session store, necessário para Devise
     config.session_store :cookie_store, key: '_interslice_session'
-
-    # Middleware para gerenciar cookies e sessões
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
-    # Remove middlewares desnecessários
     config.middleware.delete ActionDispatch::Flash
     config.middleware.delete ActionDispatch::ContentSecurityPolicy::Middleware
 
-    # Verifique se o cache do Rack::Attack está configurado
     config.after_initialize do
       Rails.logger.info "Rack::Attack cache store: #{Rack::Attack.cache.store}"
     end
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
